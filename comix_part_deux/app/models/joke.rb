91ac7ce24dbb29a389158bd7f561f39d3018e0ea
Rich <rpavesi84@gmail.com>
2019-12-02 13:13:14 -0500

    added validations for show and joke

--- comix_part_deux/app/models/joke.rb
+++ comix_part_deux/app/models/joke.rb
@@ -1,3 +1,7 @@
 class Joke < ApplicationRecord
+  validates :topic, presence: true
+  validates :punchline, presence: true
+  validates :body, presence: true
+
   belongs_to :comedian_set
 end
--- comix_part_deux/app/models/show.rb
+++ comix_part_deux/app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ApplicationRecord
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
   belongs_to :comedian
   belongs_to :club
-  
+
 end
