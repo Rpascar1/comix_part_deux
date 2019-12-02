4e5debb8e973ce3eea76fc0a04bde1c3d7691cf6
Rich <rpavesi84@gmail.com>
2019-12-02 11:10:52 -0500

    updated gem file and confirmed login still working

--- comix_part_deux/config/initializers/devise.rb
+++ comix_part_deux/config/initializers/devise.rb
@@ -292,7 +292,7 @@ Devise.setup do |config|
   # end
 
   # ==> Configuration for :registerable
-
+config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"], callback_url: "http://localhost:3000/comedians/auth/facebook/callback"
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
