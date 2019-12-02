b6c4e3d974c181b22f59b34f2428b8c023a98227
Rich <rpavesi84@gmail.com>
2019-12-02 00:47:58 -0500

    devise fb omniauth verified working after homepage test

--- comix_part_deux/config/initializers/devise.rb
+++ comix_part_deux/config/initializers/devise.rb
@@ -292,7 +292,7 @@ Devise.setup do |config|
   # end
 
   # ==> Configuration for :registerable
-config.omniauth :facebook, "604182537053744", "5fbadb63fea77a378bbc18f5b2c56571", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+config.omniauth :facebook, "604182537053744", "5fbadb63fea77a378bbc18f5b2c56571", callback_url: "http://localhost:3000/comedians/auth/facebook/callback"
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
