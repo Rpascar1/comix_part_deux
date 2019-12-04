be54efe1f22dffc8d62d0377c12c071b9b90002b
Rich <rpavesi84@gmail.com>
2019-12-04 18:31:50 -0500

    added bootstrap gem

--- comix_part_deux/app/controllers/jokes_controller.rb
+++ comix_part_deux/app/controllers/jokes_controller.rb
@@ -6,6 +6,7 @@ class JokesController < ApplicationController
 
   def create
     @joke = Joke.new(joke_params)
+    @joke.comedian = User
     if @joke.save
       flash[:success] = "The was so funny I saved it for you."
       redirect_to joke_path(@joke)
